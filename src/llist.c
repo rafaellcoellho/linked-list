40af197142e16d55145cc0901b6f7d588312b3ed
Rafael Coelho <rafaellcoellho@gmail.com>
2019-02-21 13:35:40 -0300

    Coloca implementação

--- src/llist.c
+++ src/llist.c
@@ -1,1 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "llist.h"
+
+typedef struct node* link;
+
+typedef struct node 
+{
+    uint16_t item;
+    link next;
+} node;
+
+struct llist 
+{
+    link head;
+    link tail;
+};
+
+llist *LinkedList_Create(uint16_t item) 
+{   
+    llist *self = malloc(sizeof(llist));
+
+    link head = (link) malloc(sizeof(node));
+    head->item = item;
+    head->next = NULL;
+
+    self->head = head;
+    self->tail = head;
+    return self;
+}
+
+void LinkedList_Destroy(llist *self)
+{
+    link p = self->head;
+    link aux;
+
+    while (p != NULL) {
+        aux = p->next;
+        free(p);
+        p = aux;
+    }
+    self->head = NULL;
+    self->tail = NULL;
+}
+
+void LinkedList_Prepend(llist *self, uint16_t item)
+{
+    link new_node = (link) malloc(sizeof(node));
+    new_node->item = item;
+    new_node->next = self->head;
+
+    self->head = new_node;
+}
+
+void LinkedList_Append(llist *self, uint16_t item)
+{
+    link new_node = (link) malloc(sizeof(node));
+    new_node->item = item;
+    new_node->next = NULL;
+
+    self->tail->next = new_node;
+    self->tail = new_node;
+}
+
+void LinkedList_Delete(llist *self, uint16_t item)
+{
+    // Caso seja o primeiro item, já retira e termina a função
+    link p = self->head;
+    if (p->item == item) {
+        self->head = p->next;
+        free(p);
+        return;
+    }
+
+    // Caso esteja no segundo pra frente ou não esteja na lista
+    link p_after = p->next;
+    while ( (p_after->item != item) && (p_after->next != NULL) ) {
+        p = p_after;
+        p_after = p_after->next;
+    }
+
+    // Esta na lista
+    if (p_after->item == item) {
+        if(p_after->next == NULL) p->next = NULL; // É o ultimo? 
+        else p->next = p_after->next; 
+        free(p_after);
+    } else return; // Não está na lista, então acaba sem fazer nada
+}
+
+void LinkedList_Show(llist *self)
+{
+    link p = self->head;
+
+    if(p == NULL) printf("\tEmpty Linked List");
+
+    while(p != NULL) {
+        printf("\t%d->", p->item);
+        p = p->next;
+    }
+    printf("\n");
+}
--- src/llist.h
+++ src/llist.h
@@ -1,5 +1,15 @@
 #ifndef _LLIST_H
 #define _LLIST_H
 
+#include <stdint.h>
+
+typedef struct llist llist;
+
+llist *LinkedList_Create(uint16_t item);
+void LinkedList_Destroy(llist *self);
+void LinkedList_Prepend(llist *self, uint16_t item);
+void LinkedList_Append(llist *self, uint16_t item);
+void LinkedList_Delete(llist *self, uint16_t item);
+void LinkedList_Show(llist *self);
 
 #endif // _LLIST_H
